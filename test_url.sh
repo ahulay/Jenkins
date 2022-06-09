ee804e599a57616eaca1037d1700403187c07d6c
ahulay <57831107+ahulay@users.noreply.github.com>
2022-06-09 08:43:15 +0300

    Update test_url.sh

--- test_url.sh
+++ test_url.sh
@@ -7,7 +7,7 @@ timeout=5
 
 for (( i=1; i<=$attempts; i++ ))
 do
-  code=`curl -sL --connect-timeout 20 --max-time 30 -w "%{http_code}\\n" "$url" -o /dev/null`
+  code=`curl -sL --connect-timeout 20 --max-time 30 -w "%{response_code_code}\\n" "$url" -o /dev/null`
 
   if [ "$code" = "200" ]; then
     echo "Success"
